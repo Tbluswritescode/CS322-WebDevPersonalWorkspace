2f30e17185115b6095e65500682a7577843f989d
tblus <tgblus@knox.edu>
2021-03-28 15:57:11 -0500

    Adding deploy files for deploy from laptop

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const app = Vue.createApp({
     data() {
         return {
-            Headshot: 'hs.jpg',
-            Resume: "resume.PNG",
-            titleMsg: "Hi, I'm Tristan Blus, a programming student with significant experience in Python, C++, C#, and Ruby.  I also know front end web development fundamentals, and am learning full stack development using Vue.js and python. ",
+            Headshot: '/hs.jpg',
+            Resume: "/resume.PNG",
+            opener: "Hi, I'm Tristan Blus! Welcome to my page!",
+            titleMsg: "Programming student with significant experience in Python, C++, C#, and Ruby.  I also know front end web development fundamentals, and am learning full stack development using Vue.js and python. ",
+            Introduction: "I am an aspriring software developer and a Junior at Knox College, pursuing a Bachelors of Science in Computer Science.  In addition to my studies, I have helped build multiple websites through squarespace and wordpress.  In school I have built a few very simple projects using unity, and have accumulated a significant portion of code on GitHub. I also have more than five years of experience as a theatrical lighting technician, lighting board operator, and scenic carpenter.  Feel free to investigate my programming work on...",
+            codewars: "https://www.codewars.com/users/tblus01",
+            cwimg: "cwimg.png",
+            unity: "https://play.unity.com/u/tristan-blus" ,
+            unimg: "unity.png",
             contactEmail: "tblus@outlook.com",
-            githubPage: "https://github.com/Tbluswritescode",
-            linkedIn: "https://www.linkedin.com/in/tristan-blus-4b16ab8a/"
+            github: "https://github.com/Tbluswritescode",
+            ghimg: "gh.png",
+            linkedIn: "https://www.linkedin.com/in/tristan-blus-4b16ab8a/",
+            resumeIntro: "Alternately, if you are interested in my working history in theatre technology, educational background and achievements, or volunteer experiences I've had you can find my resume below.",
+            conclusion: "More detailed resume information can be provided as requested. Feel free to contact me using the box below or at tblus@outlook.com"
+
         }
     }
 })
