0dfd78c6b8c2de7797b3d3a618a08b1d6ad56cc8
T <tblus@outlook.com>
2021-03-28 09:46:11 -0500

    adding resume and headshot

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
-            Headshot: 'hs.png',
+            Headshot: 'hs.jpg',
             Resume: "resume.png",
             titleMsg: "Hi, I'm Tristan Blus, a programming student with significant experience in Python, C++, C#, and Ruby.  I also know front end web development fundamentals, and am learning full stack development using Vue.js and python. ",
             contactEmail: "tblus@outlook.com",
