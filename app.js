67cf42c42cbb676afa902d21fd6bd44ae891bdaa
tblus <tgblus@knox.edu>
2021-03-28 09:10:53 -0500

    Adding basic vue JS work to repo

---
+++ app.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            Headshot: 'hs.png',
+            Resume: "resume.png",
+            titleMsg: "Hi, I'm Tristan Blus, a programming student with significant experience in Python, C++, C#, and Ruby.  I also know front end web development fundamentals, and am learning full stack development using Vue.js and python. ",
+            contactEmail: "tblus@outlook.com",
+            githubPage: "https://github.com/Tbluswritescode",
+            linkedIn: "https://www.linkedin.com/in/tristan-blus-4b16ab8a/"
+        }
+    }
+})
