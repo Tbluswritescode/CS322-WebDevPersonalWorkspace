cfbeb688de9a90f8b8799e3ea86e507853006359
tblus <tgblus@knox.edu>
2021-03-29 11:47:43 -0500

    final initial distribution

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 const app = Vue.createApp({
     data() {
         return {
-            Headshot: 'hs.jpg',
-            Resume: "resume.PNG",
+            Headshot: 'assets/hs.jpg',
+            Resume: "assets/resume.PNG",
             opener: "Hi, I'm Tristan Blus! Welcome to my page!",
             titleMsg: "Programming student with significant experience in Python, C++, C#, and Ruby.  I also know front end web development fundamentals, and am learning full stack development using Vue.js and python. ",
             Introduction: "I am an aspriring software developer and a Junior at Knox College, pursuing a Bachelors of Science in Computer Science.  In addition to my studies, I have helped build multiple websites through squarespace and wordpress.  In school I have built a few very simple projects using unity, and have accumulated a significant portion of code on GitHub. I also have more than five years of experience as a theatrical lighting technician, lighting board operator, and scenic carpenter.  Feel free to investigate my programming work on...",
             codewars: "https://www.codewars.com/users/tblus01",
-            cwimg: "cwimg.png",
+            cwimg: "assets/cwimg.png",
             unity: "https://play.unity.com/u/tristan-blus" ,
-            unimg: "unity.png",
+            unimg: "assets/unity.png",
             contactEmail: "tblus@outlook.com",
             github: "https://github.com/Tbluswritescode",
-            ghimg: "gh.png",
+            ghimg: "assets/gh.png",
             linkedIn: "https://www.linkedin.com/in/tristan-blus-4b16ab8a/",
             resumeIntro: "Alternately, if you are interested in my working history in theatre technology, educational background and achievements, or volunteer experiences I've had you can find my resume below.",
             conclusion: "More detailed resume information can be provided as requested. Feel free to contact me using the box below or at tblus@outlook.com"
